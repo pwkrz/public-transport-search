02eadb9804237b1e1a753f68231498fa1df62964
pwkrz <pg.krzeminski@gmail.com>
2021-01-19 17:04:29 +0100

    added getStartLocation method

--- src/app/services/data-storage.service.ts
+++ src/app/services/data-storage.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Suggestion } from '../models/suggestion.int';
 
+enum LocalStorageItems {
+  StartLocation = 'startLocation',
+  Routes = 'routes'
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,11 +16,22 @@ export class DataStorageService {
   saveStartLocation(s: Suggestion): Promise<any> {
     return new Promise((resolve, reject) => {
       if (!!localStorage) {
-        localStorage.setItem('startLocation', JSON.stringify(s));
+        localStorage.setItem(LocalStorageItems.StartLocation, JSON.stringify(s));
         resolve(true);
       } else {
         reject('localStorage API not found. Please update your browser.');
       }
     });
+  }
+
+  getStartLocation(): Suggestion | false {
+    const startLocationString = localStorage.getItem(LocalStorageItems.StartLocation);
+    if (startLocationString) {
+      const startLocation = JSON.parse(startLocationString);
+      console.log(startLocation.constructor);
+      return startLocation;
+    } else {
+      return false;
+    }
   }
 }

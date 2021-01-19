097239aedce582be27976757ff055a14656e6797
pwkrz <pg.krzeminski@gmail.com>
2021-01-19 16:21:55 +0100

    moved HTML template to seprate file

--- src/app/views/navbar/navbar.component.ts
+++ src/app/views/navbar/navbar.component.ts
@@ -2,21 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  template: `
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-      <span class="navbar-brand">Public Transport Search</span>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-        </ul>
-      </div>
-      <span>cokolwiek</span>
-    </div>
-  </nav>
-  `,
+  templateUrl: './navbar.component.html',
   styles: [
   ]
 })

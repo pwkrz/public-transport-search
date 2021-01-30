1e08ab3922cfad40a0ddec7c5b71affe4e61cff0
pwkrz <pg.krzeminski@gmail.com>
2021-01-30 13:45:27 +0100

    one more fix post-merge

--- src/app/public-transport-search/public-transport-search.module.ts
+++ src/app/public-transport-search/public-transport-search.module.ts
@@ -1,3 +1,4 @@
+import { RouteSuggestionsModule } from './components/route-suggestions/route-suggestions.module';
 import { PTSRouting } from './p-t-s.routing';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -14,6 +15,7 @@ import { PlaceSelectorModule } from './components/place-selector/place-selector.
   imports: [
     CommonModule,
     PlaceSelectorModule,
+    RouteSuggestionsModule,
     PTSRouting,
   ],
   exports: [PublicTransportSearchComponent]

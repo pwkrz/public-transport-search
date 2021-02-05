dd40d822997039e2738148a8664b4be0026071fc
pwkrz <pg.krzeminski@gmail.com>
2021-02-05 16:58:02 +0100

    beforeDismiss callback for modal + code cleanup

--- src/app/public-transport-search/views/destination/destination-selector.component.ts
+++ src/app/public-transport-search/views/destination/destination-selector.component.ts
@@ -2,7 +2,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { Suggestion } from '../../models/suggestion.int';
 import { DataStreamsService } from '../../services/data-streams.service';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 import { RouteSuggestionsComponent } from '../../components/route-suggestions/route-suggestions.component';
 
 @Component({
@@ -18,14 +18,23 @@ export class DestinationSelectorComponent {
               private router: Router,
               private activatedRoute: ActivatedRoute,
               private modalService: NgbModal) {
-    this.dataStreamsService.getStartLocationStream()
-                    .subscribe(start => this.startLocation = start);
-    this.activatedRoute.queryParams
-                    .subscribe(qParams => {
+
+    const MODAL_OPTIONS: NgbModalOptions = {
+      size: 'lg',
+      scrollable: true,
+      beforeDismiss: () => {
+        this.router.navigate([]);
+        return true;
+      }
+    };
+
+    this.dataStreamsService.getStartLocationStream().subscribe(start => this.startLocation = start);
+
+    this.activatedRoute.queryParams.subscribe(qParams => {
       this.modalService.dismissAll();
       if (qParams.place_id && qParams.name) {
         this.destination = qParams;
-        const modalRef = this.modalService.open(RouteSuggestionsComponent, {size: 'lg', scrollable: true});
+        const modalRef = this.modalService.open(RouteSuggestionsComponent, MODAL_OPTIONS);
         modalRef.componentInstance.startID = this.startLocation.place_id;
         modalRef.componentInstance.endID = this.destination.place_id;
         modalRef.componentInstance.destinationName = this.destination.name;
